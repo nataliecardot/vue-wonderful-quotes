c3db5f5d02e733d252975a61a38b24dd6dd5e0bb
Natalie-Cardot <nataliecardot@gmail.com>
2020-09-14 21:11:38 -0700

    Pass data with props and slots

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,11 +1,19 @@
 <template>
   <!-- Bootstrap container class is in App.vue -->
-  <div class="row"></div>
+  <div class="row">
+    <app-quote v-for="quote in quotes" :key="quote">{{ quote }}</app-quote>
+  </div>
 </template>
 
 <script>
+import Quote from "./Quote.vue";
+
 export default {
+  // props are passed into QuoteGrid from App.vue file
   props: ["quotes"],
+  components: {
+    appQuote: Quote,
+  },
 };
 </script>
 
