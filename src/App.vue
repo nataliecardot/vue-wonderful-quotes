5dd8065ee388383859e3fc59fe532f618cd7f458
Natalie-Cardot <nataliecardot@gmail.com>
2020-09-17 21:10:25 -0700

    Allow for quote deletion

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- newQuote($event) is implicitly passed, where $event refers to data passed in custom event -->
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     <div class="row">
       <div class="col-sm-12 text-center">
         <div class="alert alert-info">Click on a quote to delete it.</div>
@@ -26,6 +26,10 @@ export default {
     // Vue.js automatically provides data passed on custom event, so can simply access the quote passed by NewQuote.vue in the emit event in the argument
     newQuote(quote) {
       this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      // Modify existing array by removing one element at index passed by custom emit event on QuoteGrid.vue
+      this.quotes.splice(index, 1);
     },
   },
   components: {
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,7 +1,12 @@
 <template>
   <!-- Bootstrap container class is in App.vue -->
   <div class="row">
-    <app-quote v-for="quote in quotes" :key="quote">{{ quote }}</app-quote>
+    <!-- native modifier tells Vue.js to react to click on component if it happens on native element of this component (the outer div in Quote component template). It says, even though it happened on the HTML code of the component, treat is as if it were a click on app-quote element here (which is not appended to the DOM, only the code in the template is; this workaround is needed). So, it treats clicks on the template as clicks on the component -->
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="quote"
+      @click.native="deleteQuote(index)"
+    >{{ quote }}</app-quote>
   </div>
 </template>
 
@@ -13,6 +18,11 @@ export default {
   props: ["quotes"],
   components: {
     appQuote: Quote,
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    },
   },
 };
 </script>
