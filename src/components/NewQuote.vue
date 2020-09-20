20e83b380e1e05567e2ee0fb26c662c124f1fb76
Natalie-Cardot <nataliecardot@gmail.com>
2020-09-20 00:28:38 -0700

    Add trim modifier

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -4,7 +4,7 @@
     <form action>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label for="quote">Quote</label>
-        <textarea id="quote" rows="3" class="form-control" v-model="quote"></textarea>
+        <textarea id="quote" rows="3" class="form-control" v-model.trim="quote"></textarea>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <!-- Using prevent modifier because don't want to submit the form; don't want to send request to server, which would reload page -->
